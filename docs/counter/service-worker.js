48d0c4c4d85695268199e0e8a78656b1e97b9e18
leelan324 <liran310303@gmail.com>
2024-03-04 20:04:34 +0900

    2024/03/04

---
+++ docs/counter/service-worker.js
@@ -0,0 +1,42 @@
+//キャッシュ名
+const CACHE_NAME  = "カウンター-PWA";
+
+//キャッシュするファイル名
+const urlsToCache = [
+	'apple-touch-icon.png',	
+	'favicon.ico',
+	'icon192.png',
+	'icon256.png',
+	'index.html',
+	'manifest.json',
+	'service-worker.js',
+];
+
+//インストール時処理
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches
+        .open(CACHE_NAME)
+        .then(function(cache){
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+// フェッチ時のキャッシュロード処理
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches
+            .match(event.request)
+            .then(function(response) {
+                if(response){
+                    return response;
+                }
+                return fetch(event.request);
+            })
+    );
+});
+
+self.addEventListener('message', function (event) {
+    self.registration.showNotification(event.data);
+});
